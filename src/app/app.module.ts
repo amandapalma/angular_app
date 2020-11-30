9f4371dfb55f252c3b96155efe77e68aaed26f7d
Palma Avecilla <amanda.palma-avecilla@capgemini.com>
2020-11-30 12:23:22 +0100

    add basic components structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Landing } from './core/landing/landing.component';
+import { SiteComponent } from './core/site/site.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { RouterComponent } from './core/site/router/router.component';
+import { CatalogComponent } from './core/site/router/catalog/catalog.component';
+import { ItemDetailsComponent } from './core/site/router/item-details/item-details.component';
+import { CartComponent } from './core/site/router/cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Landing
+    Landing,
+    SiteComponent,
+    HeaderComponent,
+    RouterComponent,
+    CatalogComponent,
+    ItemDetailsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
