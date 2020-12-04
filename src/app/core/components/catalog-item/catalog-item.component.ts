ef5ab26b82561383a041aba01b6ff75e8322049e
Palma Avecilla <amanda.palma-avecilla@capgemini.com>
2020-12-04 12:23:30 +0100

    paint catalog items v.1

--- src/app/core/components/catalog-item/catalog-item.component.ts
+++ src/app/core/components/catalog-item/catalog-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CatalogItem} from '../../../shared/models/catalog-item.model';
 
 @Component({
   selector: 'app-catalog-item',
@@ -6,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./catalog-item.component.css']
 })
 export class CatalogItemComponent implements OnInit {
+constructor() { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+ngOnInit(): void {
   }
 
 }
--- src/app/core/containers/catalog/catalog.component.ts
+++ src/app/core/containers/catalog/catalog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CatalogItem} from '../../../shared/models/catalog-item.model';
 
 @Component({
   selector: 'app-catalog',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./catalog.component.css']
 })
 export class CatalogComponent implements OnInit {
+  catalogItems: CatalogItem[] = [new CatalogItem ('mascarilla', 'evita la inhalación de ciertos gases o sustancias', 'https://cdn.pixabay.com/photo/2020/03/24/16/17/mask-4964590_960_720.png'), new CatalogItem ('lavado de manos', 'elimina restos, suciedad y microorganismos', 'https://www.mmhealth.org/wp-content/uploads/Handwashing-Web.jpg')];
 
-  constructor() { }
+constructor() { }
 
-  ngOnInit(): void {
+ngOnInit(): void {
   }
 
 }
+
